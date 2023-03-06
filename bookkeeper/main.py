9e579ba48979c4c1b04005c03ce7ce2a041ad2bc
Roman Oryol <oryol.rd@gmail.com>
2023-03-06 17:48:32 +0300

    bottom controls done

--- bookkeeper/main.py
+++ bookkeeper/main.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QVBoxLayout, QLabel, QWidget, QGridLayout
+from PySide6.QtWidgets import QVBoxLayout, QLabel, QWidget, QGridLayout, QComboBox, QLineEdit, QPushButton
 
 from bookkeeper.models.category import Category
 from bookkeeper.models.expense import Expense
@@ -18,21 +18,21 @@ class TableModel(QtCore.QAbstractTableModel):
         super(TableModel, self).__init__()
         self._data = data
 
-        def data(self, index, role):
-            if role == Qt.DisplayRole:
-                # See below for the nested-list data structure.
-                # .row() indexes into the outer list,
-                # .column() indexes into the sub-list
-                return self._data[index.row()][index.column()]
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            # See below for the nested-list data structure.
+            # .row() indexes into the outer list,
+            # .column() indexes into the sub-list
+            return self._data[index.row()][index.column()]
 
-            def rowCount(self, index):
-                # The length of the outer list.
-                return len(self._data)
+    def rowCount(self, index):
+        # The length of the outer list.
+        return len(self._data)
 
-            def columnCount(self, index):
-                # The following takes the first sub-list, and returns
-                # the length (only works if all rows are an equal length)
-                return len(self._data[0])
+    def columnCount(self, index):
+        # The following takes the first sub-list, and returns
+        # the length (only works if all rows are an equal length)
+        return len(self._data[0])
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -55,14 +55,23 @@ class MainWindow(QtWidgets.QMainWindow):
         bottom_controls = QGridLayout()
 
         bottom_controls.addWidget(QLabel('Сумма'), 0, 0)
-        bottom_controls.addWidget(QLabel('=============='), 0, 1)
-        bottom_controls.addWidget(QLabel('=============='), 0, 2)
+
+        amount_line_edit = QLineEdit()
+
+        bottom_controls.addWidget(amount_line_edit, 0, 1)
+        #bottom_controls.addWidget(QLabel('=============='), 0, 2)
         bottom_controls.addWidget(QLabel('Категория'), 1, 0)
-        bottom_controls.addWidget(QLabel('======Продукты======'), 1, 1)
-        bottom_controls.addWidget(QLabel('======Редактировать======'), 1, 2)
-        bottom_controls.addWidget(QLabel('============'), 2, 0)
-        bottom_controls.addWidget(QLabel('========Добавить======='), 2, 1)
-        bottom_controls.addWidget(QLabel('========Добавить======='), 2, 2)
+
+        category_dropdown = QComboBox()
+        category_dropdown.addItems(['Продукты', 'Тест', 'Тест'])
+
+        bottom_controls.addWidget(category_dropdown, 1, 1)
+
+        category_edit_button = QPushButton('Редактировать')
+        bottom_controls.addWidget(category_edit_button, 1, 2)
+
+        expense_add_button = QPushButton('Добавить')
+        bottom_controls.addWidget(expense_add_button, 2, 1)
 
         bottom_widget = QWidget()
         bottom_widget.setLayout(bottom_controls)
